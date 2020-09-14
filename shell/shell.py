bb125798661bdcb4c71839b5d73025fe70bef89b
castro3 <castroxoscar@gmail.com>
2020-09-14 02:16:44 -0600

    Deleted shell project folder and merged fork/exec/wait into a single file called shell.py

---
+++ shell/shell.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+
+import os, sys, time, re
+
+while 1:
+    userInput = input(os.getcwd() + "$")
+    userArgs = userInput.split(" ")
+
+    # if user doesn't input anything, we prompt again
+    if "" in userArgs:
+        continue
+
+
+    # end shell if user enters exit
+    if 'exit' in userArgs:
+        sys.exit(-1)
+
+    # change directory when cd is in user input
+    if 'cd' in userArgs:
+        if '..' in userArgs:
+            dirChange = '..'
+        else:
+            dirChange = userArgs[0]
+            try:
+                os.chdir(dirChange) # attempt to change dir
+
+            except FileNotFoundError:
+                pass
+
+            continue
+
+        
+    pid = os.getpid()
+    os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+
+    elif rc == 0:
+
+        os.write(1, ("Child: pid ==%d. Parent: pid=%d\n" %(os.getpid(), pid)).encode())
+            
+    else:
+        os.write(1, ("Parent: pid=%d. Child's pid = %d\n" %(pid, rc)).encode())
+        childPid = os.wait()
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" %childPid).encode())
+                
